d12411a463267832f51691640c120cb0ff45e5b3
suraj <kumarsuraj.suraj151@gmail.com>
2022-10-12 08:02:04 +0530

    pingla update

---
+++ pingala/codes/2_5.py
@@ -0,0 +1,33 @@
+# Plotting y(n)
+
+# Name: Ankit Saha
+# Roll number: AI21BTECH11004
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy as sp
+
+def x(n):
+    if n < 0:
+        return 0
+    elif n <= 1:
+        return 1
+    else:
+        return x(n-1) + x(n-2)
+
+def y(n):
+    if n < 0:
+        return 0
+    else:
+        return x(n-1) + x(n+1)
+
+N = 20
+n_arr = np.arange(N)
+y_vec = sp.vectorize(y)
+
+plt.stem(n_arr, y_vec(n_arr))
+plt.grid()
+plt.xlabel('$n$')
+plt.ylabel('$y(n)$')
+plt.savefig('../figs/fig-2.png')
+plt.show()
