df1b982e91715176dd3aa8981f87d8f7ea34554c
suraj <kumarsuraj.suraj151@gmail.com>
2022-10-13 12:57:27 +0530

    added code for pingala

--- pingala/codes/1_1.py
+++ pingala/codes/1_1.py
@@ -17,64 +17,68 @@ def f1(n,a,b):
 a=(1+math.sqrt(5))/2
 b=(1-math.sqrt(5))/2
 #1.1
+
 n=np.arange(1,10)
+
 vec_an=scipy.vectorize(an)
+
+
 def f2(n,a,b):
     return np.sum(vec_an(np.arange(1,n+1),a,b))
 
-vec_f1=scipy.vectorize(f1)
-vec_f2=scipy.vectorize(f2)
-l1=vec_f1(n,a,b)
-l2=vec_f2(n,a,b)
-plt.stem(n,l1,label=r'$a_{n+2}-1$',markerfmt='go')
-plt.grid()
-plt.legend()
-plt.stem(n,l2,label=r'$\sum_{k=1}^{n}a_{k}$')
-plt.grid()
-plt.legend()
+# vec_f1=scipy.vectorize(f1)
+# vec_f2=scipy.vectorize(f2)
+# l1=vec_f1(n,a,b)
+# l2=vec_f2(n,a,b)
+# plt.stem(n,l1,label=r'$a_{n+2}-1$',markerfmt='go')
+# plt.grid()
+# plt.legend()
+# plt.stem(n,l2,label=r'$\sum_{k=1}^{n}a_{k}$')
+# plt.grid()
+# plt.legend()
 # plt.savefig('../figs/1_1.png')
-plt.show()
+# plt.show()
 
-
-
-
-def f3(n,a,b):
-   return np.dot(vec_an(np.arange(n),a,b),np.array([1/10**i for i in range(n)]))-10/89
+# def f3(n,a,b):
+#    return np.dot(vec_an(np.arange(n),a,b),np.array([1/10**i for i in range(n)]))-10/89
 
 
 
 
 
 #1.2
-vec_f3=scipy.vectorize(f3)
-l3=vec_f3(n,a,b)
-plt.stem(n,l3,label=r'$\sum_{k=1}^{n}\frac{a_{k}}{10^k}-(\frac{10}{89})$')
-plt.legend()
-plt.grid()
-plt.savefig('')
-plt.show()
+# vec_f3=scipy.vectorize(f3)
+# l3=vec_f3(n,a,b)
+# plt.stem(n,l3,label=r'$\sum_{k=1}^{n}\frac{a_{k}}{10^k}-(\frac{10}{89})$')
+# plt.legend()
+# plt.grid()
+# plt.savefig('../figs/1_2.png')
+# # plt.show()
 
 
 
 
-#1.3
-def f4(n,a,b):
-    return a**n+b**n
+
+
+# #1.3
+# def f4(n,a,b):
+#     return a**n+b**n
+# vec_f4=scipy.vectorize(f4)
+# vec_bn=scipy.vectorize(bn)
+# l4=vec_bn(n,a,b)
+# l5=vec_f4(n,a,b)
+# plt.stem(n,l4,label=r'$b_{n}$',markerfmt='go')
+# plt.grid()
+# plt.legend()
+# plt.stem(n,l5,label=r'$\alpha^n+\beta^n$')
+# plt.grid()
+# plt.legend()
+# plt.savefig('../figs/1_3.png')
+# plt.show()
+
+
+1.4
 vec_bn=scipy.vectorize(bn)
-vec_f4=scipy.vectorize(f4)
-l4=vec_bn(n,a,b)
-l5=vec_f4(n,a,b)
-# plt.subplot(211)
-plt.stem(n,l4,label=r'$b_{n}$')
-plt.grid()
-plt.legend()
-# plt.subplot(212)
-plt.stem(n,l5,label=r'$\alpha^n+\beta^n$')
-plt.grid()
-plt.legend()
-plt.savefig('../figs/1_3.png')
-plt.show()
-#1.4
 def f5(n,a,b):
    return np.dot(vec_bn(np.arange(n),a,b),np.array([1/10**i for i in range(n)]))-8/89
 vec_f5=scipy.vectorize(f5)
